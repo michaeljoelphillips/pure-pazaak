773157a9833c36833eb821e29679043299791dd7
Michael Phillips <michaeljoelphillips@gmail.com>
2017-12-02 12:09:28 -0600

    Remove old test class and cleanup docs for Main.java.

--- src/main/java/com/gamebros/purepazaak/Main.java
+++ src/main/java/com/gamebros/purepazaak/Main.java
@@ -3,7 +3,17 @@ package com.gamebros.purepazaak;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
+/**
+ * The Main Class for Pure Pazaak.
+ */
 public class Main {
+  /**
+   * The entrypoint for our application.
+   *
+   * @param String[] args
+   * @throws SlickException
+   * @return void
+   */
   public static void main(String[] args) throws SlickException {
     AppGameContainer app = new AppGameContainer(new PurePazaak());
     app.setDisplayMode(800, 600, false);
