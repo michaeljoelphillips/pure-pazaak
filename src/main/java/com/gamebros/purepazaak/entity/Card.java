34c88b1a5e4ae473fd548d1e0c4105ffe8e53cc9
Ryan Phillips <ryan.phillips.j@gmail.com>
2017-12-03 22:29:33 -0600

    Fixed Spacing between Methods.

--- src/main/java/com/gamebros/purepazaak/entity/Card.java
+++ src/main/java/com/gamebros/purepazaak/entity/Card.java
@@ -4,11 +4,8 @@ public class Card implements CardInterface {
         protected int value;
         public Card(int value){
             this.value = value;
-
         }
         public int getValue() {
             return value;
         }
-
-
 }
