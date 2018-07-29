5e83ba605b225965718a5e006735838c2d1fb249
Michael Phillips <michaeljoelphillips@gmail.com>
2018-07-29 15:44:12 -0500

    Run the game in 1080p with a real background.

--- java/src/main/java/com/github/nomad145/purepazaak/PurePazaak.kt
+++ java/src/main/java/com/github/nomad145/purepazaak/PurePazaak.kt
@@ -5,10 +5,18 @@ import playn.java.JavaPlatform;
 import playn.java.LWJGLPlatform;
 
 fun main(args: Array<String>) {
-  val config = JavaPlatform.Config()
-  val platform = LWJGLPlatform(config)
+  val platform = LWJGLPlatform(buildConfig())
 
   PurePazaak(platform, 60)
 
   platform.start()
+}
+
+fun buildConfig(): JavaPlatform.Config {
+  var config = JavaPlatform.Config()
+
+  config.width = 1920
+  config.height = 1080
+
+  return config
 }
